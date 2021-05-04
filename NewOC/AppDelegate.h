48370d97ed0cccf0f70493966e943468646cf8ce
纵昂 <zongang15@yahoo.com>
2021-05-04 18:18:01 +0800

    Initial Commit

---
+++ NewOC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NewOC
+//
+//  Created by 纵昂 on 2021/5/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NewOC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  NewOC
+//
+//  Created by 纵昂 on 2021/5/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
